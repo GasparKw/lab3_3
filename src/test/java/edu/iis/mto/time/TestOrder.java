d6a8f834b992636110d89eea427a12914e3e7489
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-04-27 16:22:02 +0200

    Stworzono klase testujaca

---
+++ src/test/java/edu/iis/mto/time/TestOrder.java
@@ -0,0 +1,16 @@
+package edu.iis.mto.time;
+
+import edu.iis.mto.sourcetime.*;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestOrder {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
